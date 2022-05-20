c8eb07656e50531c59f12c3c3023749bae80a3ad
Stanislav Finko <ghostzip8@gmail.com>
2022-05-20 17:17:40 +0300

    Update src/accounts/views.py

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+
+class AccountView(LoginRequiredMixin, TemplateView):
+    template_name='accounts/account_page.html'
+    login_url='login/'
+
+
+# class AdminView(PermissionRequiredMixin, TemplateView):
+#     template_name='accounts/admin.html'
+#     permission_required='is_admin'
+#     raise_exception = True  # Raise exception when no access instead of redirect
+#     permission_denied_message = "You are not an admin!"
+
+
+class LoginView(TemplateView):
+    template_name='accounts/login.html'
+
+
+class RegisterView(TemplateView):
+    template_name='accounts/register.html'
