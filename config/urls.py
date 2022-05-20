b2b3bbd40caa4e208f11711c2a238278d66ac0be
Stanislav Finko <ghostzip8@gmail.com>
2022-05-20 17:19:51 +0300

    Create new html files for accounts app. Update urls and welcome.html.

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('src.home.urls'))
+    path('', include('src.home.urls')),
+    path('account/', include('src.accounts.urls')),
+    
 ]
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 
 urlpatterns = [
-    
+    path('', AccountView.as_view(), name='account'),
+    # path('admin/', ),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 
 ]
