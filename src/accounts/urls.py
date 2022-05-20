cb7d2337591e1a1dfcf132a2d8e6cb09ab8b4037
Stanislav Finko <ghostzip8@gmail.com>
2022-05-20 20:01:34 +0300

    Trying to use Django auth forms.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', AccountView.as_view(), name='account'),
     # path('admin/', ),
     path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
+    #path('register/', RegisterView.as_view(), name='register'),
+    path('register/', registerPage, name='register'),
 
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,11 @@
+from multiprocessing import context
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from .forms import CreateUserForm
 
 
 class AccountView(LoginRequiredMixin, TemplateView):
@@ -20,3 +26,17 @@ class LoginView(TemplateView):
 
 class RegisterView(TemplateView):
     template_name='accounts/register.html'
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = CreateUserForm()
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
