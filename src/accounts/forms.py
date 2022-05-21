e11400aec7aa058ab87377a4146d7479fbb6f743
Stanislav Finko <ghostzip8@gmail.com>
2022-05-21 20:00:22 +0300

    Rewrite django registration forms.

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -12,3 +12,33 @@ class CreateUserForm(UserCreationForm):
             'password2',
 
         ]
+
+    username = forms.CharField(
+        max_length = 30,
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Username',
+            }
+        )
+    )
+
+    password1 = forms.CharField(
+        max_length = 30,
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Password',
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        max_length = 30,
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Re-enter the password',
+            }
+        )
+    )
